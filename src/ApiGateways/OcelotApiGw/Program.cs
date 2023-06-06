0fe39b150f9fcfbbe4dd336b87be2fee03955a1d
Nikhil <nikhilbanuri4@gmail.com>
2023-06-06 21:58:44 +0530

    Building API Gateways with Ocelot and Applying Gateway Routing Pattern

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,17 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging")).AddConsole().AddDebug();
+builder.Services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
+
+app.Run();
